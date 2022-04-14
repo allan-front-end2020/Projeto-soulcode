2942c298323a98e47e1dc3808061872a687343ef
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-04-14 10:27:15 -0300

    page novo Funcionario feito

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,8 +2,14 @@ import  { NgModule } from'@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes =[
 
+const routes: Routes =[
+    {
+    path:'funcionario',
+    loadChildren:()=> import('./funcionario/funcionario.module').then ((m)=> {
+    return m.FuncionarioModule
+    })
+}
 ]
 
 @NgModule({
